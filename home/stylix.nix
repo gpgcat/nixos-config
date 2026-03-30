568dff4f96617222eb9569afe54a8841a271c877
gpgcat <gpgcat@proton.me>
2026-03-30 17:44:42 +0000

    home: fix zen fonts

--- home/stylix.nix
+++ home/stylix.nix
@@ -7,6 +7,25 @@
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
+    fonts = {
+      serif = {
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Serif";
+      };
+      sansSerif = {
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Sans";
+      };
+      monospace = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      emoji = {
+        package = pkgs.twemoji-color-font;
+        name = "Twitter Color Emoji";
+      };
+    };
+
     image = config.lib.stylix.pixel "base00";
 
     targets = {
