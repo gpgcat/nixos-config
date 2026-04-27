0bf5b50a04257f29e86c8305b562246a68996f55
gpgcat <gpgcat@proton.me>
2026-04-27 23:59:53 +0300

    tidy

--- system/vanilla/default.nix
+++ system/vanilla/default.nix
@@ -3,6 +3,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./disko.nix
     ];
 
   services.xserver.videoDrivers = [ "modesetting" ];
--- system/vanilla/hardware-configuration.nix
+++ system/vanilla/hardware-configuration.nix
@@ -10,40 +10,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/mapper/cryptroot";
-      fsType = "btrfs";
-      options = [ "subvol=@" ];
-    };
-
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/16a1cea4-e88a-4b85-9d9f-1e420dd8f436";
-
-  fileSystems."/.snapshots" =
-    { device = "/dev/mapper/cryptroot";
-      fsType = "btrfs";
-      options = [ "subvol=@snapshots" ];
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/mapper/cryptroot";
-      fsType = "btrfs";
-      options = [ "subvol=@home" ];
-    };
-
-  fileSystems."/nix" =
-    { device = "/dev/mapper/cryptroot";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" ];
-    };
-
-  fileSystems."/swap" =
-    { device = "/dev/mapper/cryptroot";
-      fsType = "btrfs";
-      options = [ "subvol=@swap" ];
-    };
-
-  swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
