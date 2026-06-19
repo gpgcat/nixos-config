778a76e1d415390d82391c54685be73b1a1c40f1
gpgcat <gpgcat@proton.me>
2026-06-19 03:10:19 +0300

    fix(env): add NIXOS_OZONE_WL=1

--- home/globals.nix
+++ home/globals.nix
@@ -3,12 +3,15 @@ let
   sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
+    NIXOS_OZONE_WL = "1";
   };
 in
 {
-  home.username = "madeline";
-  home.homeDirectory = "/home/madeline";
-  home.stateVersion = "26.05";
+  home = {
+    username = "madeline";
+    homeDirectory = "/home/madeline";
+    stateVersion = "26.05";
+  };
 
   home.sessionVariables = sessionVariables;
   systemd.user.sessionVariables = sessionVariables;
