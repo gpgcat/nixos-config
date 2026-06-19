3e1b61e1a799c37e3ab1e93ad5f23d2ca0747af4
gpgcat <gpgcat@proton.me>
2026-06-19 18:00:26 +0300

    feat(home): add xdg portals

--- home/globals.nix
+++ home/globals.nix
@@ -21,6 +21,21 @@ in
     };
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+    config = {
+      common = {
+        default = [ "gtk" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+      };
+    };
+  };
+
   home.sessionVariables = sessionVariables;
   systemd.user.sessionVariables = sessionVariables;
 }
