f2b9eaa3d29356b09a7317f82a65d85c1476a632
gpgcat <gpgcat@proton.me>
2026-06-22 00:57:36 +0300

    fix(sops): use nix paths

--- hosts/azuki/sops.nix
+++ hosts/azuki/sops.nix
@@ -3,7 +3,7 @@
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   sops = {
-    defaultSopsFile = toString flakeRoot + "/secrets/azuki.yaml";
+    defaultSopsFile = flakeRoot + /secrets/azuki.yaml;
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
     secrets = {
