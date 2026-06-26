ae6a939dad9e514f84983036340321b1fdf26c37
gpgcat <gpgcat@proton.me>
2026-06-26 18:56:57 +0300

    feat(system): greetd

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   services = {
     pipewire = {
       enable = true;
       pulse.enable = true;
+    };
+
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${lib.getExe pkgs.tuigreet} --time --remember --asterisks --remember-user-session --cmd niri-session";
+          user = "greeter";
+        };
+      };
     };
 
     openssh.enable = true;
